a72dd4bc94afba7086480a4e6808ddc15ee11450
g4mer9 <rileyfinkgamer9@gmail.com>
2024-10-10 12:02:43 -0700

    step 1 completed

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,18 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My incredible game";
+const gameName = "My button mashing game";
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+//used brace for this https://chat.brace.tools/s/aa2a1e9a-e4f8-44fb-9dd7-385748f720b5
+const button: HTMLButtonElement = document.createElement('button');
+button.innerHTML = "Mash Button 🕹️";
+button.id = "Button Masher";
+button.style.color = "black"
+button.style.backgroundColor = "white";
+app.append(button);
+
