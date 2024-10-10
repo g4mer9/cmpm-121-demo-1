7a16a0b7bc408e2e6b206dbf5cfadb07d467e26d
g4mer9 <rileyfinkgamer9@gmail.com>
2024-10-10 12:05:41 -0700

    step 1 completed but now with style checks

--- src/main.ts
+++ src/main.ts
@@ -10,10 +10,9 @@ header.innerHTML = gameName;
 app.append(header);
 
 //used brace for this https://chat.brace.tools/s/aa2a1e9a-e4f8-44fb-9dd7-385748f720b5
-const button: HTMLButtonElement = document.createElement('button');
+const button: HTMLButtonElement = document.createElement("button");
 button.innerHTML = "Mash Button 🕹️";
 button.id = "Button Masher";
-button.style.color = "black"
+button.style.color = "black";
 button.style.backgroundColor = "white";
 app.append(button);
-
