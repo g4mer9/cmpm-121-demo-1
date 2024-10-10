2e622ec88b016e104975f84082bc7d548d975926
g4mer9 <rileyfinkgamer9@gmail.com>
2024-10-10 14:55:36 -0700

    step 2 with style

--- src/main.ts
+++ src/main.ts
@@ -14,13 +14,10 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-
-
 function incrementButtons() {
-    counter++;
-    resourceValueCounter.innerHTML = `${counter} buttons mashed`;
+  counter++;
+  resourceValueCounter.innerHTML = `${counter} buttons mashed`;
 }
-
 
 //used brace for this https://chat.brace.tools/s/aa2a1e9a-e4f8-44fb-9dd7-385748f720b5
 const button: HTMLButtonElement = document.createElement("button");
@@ -33,4 +30,4 @@ app.append(button);
 document.body.append(resourceValue);
 
 const b = document.getElementById("Button Masher");
-if(b) b.addEventListener("click", incrementButtons, false)
+if (b) b.addEventListener("click", incrementButtons, false);
