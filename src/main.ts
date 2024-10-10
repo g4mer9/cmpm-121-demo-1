961d4ca831323609bc93335ae6ad6e8c59ad7068
g4mer9 <rileyfinkgamer9@gmail.com>
2024-10-10 15:47:39 -0700

    new step, but needs style

--- src/main.ts
+++ src/main.ts
@@ -14,9 +14,24 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-function incrementButtons() {
-  counter++;
-  resourceValueCounter.innerHTML = `${counter} buttons mashed`;
+//used mdn docs example for this part https://developer.mozilla.org/en-US/docs/Web/API/Window/requestAnimationFrame#examples
+let start: DOMHighResTimeStamp;
+let lastElapsed: DOMHighResTimeStamp = 0;
+function incrementButtonsTime(timestamp: DOMHighResTimeStamp) {
+    if(start === undefined) start = timestamp;
+    const elapsed: DOMHighResTimeStamp = timestamp - start;
+    if(elapsed - lastElapsed > 1000 && elapsed != 0) {
+        counter++;
+        resourceValueCounter.innerHTML = `${counter} buttons mashed`;
+        lastElapsed = elapsed;
+    }
+    
+    requestAnimationFrame(incrementButtonsTime);
+}
+
+function incrementButtonsClicks() {
+    counter++;
+    resourceValueCounter.innerHTML = `${counter} buttons mashed`;
 }
 
 //used brace for this https://chat.brace.tools/s/aa2a1e9a-e4f8-44fb-9dd7-385748f720b5
@@ -30,5 +45,7 @@ app.append(button);
 document.body.append(resourceValue);
 
 const b = document.getElementById("Button Masher");
-if (b) b.addEventListener("click", incrementButtons, false);
-setInterval(incrementButtons, 1000);
+if (b) b.addEventListener("click", incrementButtonsClicks, false);
+//setInterval(incrementButtons, 1000);
+
+requestAnimationFrame(incrementButtonsTime);
