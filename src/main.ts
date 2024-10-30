c422df1f70e2b5cc6c8fd277d9ae4c0e11e7a3b6
g4mer9 <rileyfinkgamer9@gmail.com>
2024-10-29 17:18:28 -0700

    D1.d done

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,60 @@
 import "./style.css";
 
 interface Item {
-  name: string,
-  description: string,
-  cost: number,
-  rate: number,
-  count: number
+  name: string;
+  description: string;
+  cost: number;
+  rate: number;
+  count: number;
 }
 
 const availableItems: Item[] = [
-  {name: "Controller 🕹️", description: "Standard controller for mashing purposes. The buttons are already worn down.", cost: 10, rate: 0.1, count: 0},
-  {name: "Button Mashing Grandma 👵", description: "Poached from the cookie farm layoffs, worse than you at mashing.", cost: 100, rate: 2, count: 0},
-  {name: "Turbo Controller ⏩", description: "Makes your grandmas more efficient by replacing mashing buttons with holding them.", cost: 1000, rate: 50, count: 0},
-  {name: "Button Mashing Machine 🤖", description: "Much faster than you or your grandmas at mashing those buttons.", cost: 2000, rate: 100, count: 0},
-  {name: "Ludwig 👨", description: "Self-proclaimed best button masher in the world.", cost: 10000, rate: 1000, count: 0},
+  {
+    name: "Controller 🕹️",
+    description:
+      "Standard controller for mashing purposes. The buttons are already worn down.",
+    cost: 10,
+    rate: 0.1,
+    count: 0,
+  },
+  {
+    name: "Button Mashing Grandma 👵",
+    description:
+      "Poached from the cookie farm layoffs, worse than you at mashing.",
+    cost: 100,
+    rate: 2,
+    count: 0,
+  },
+  {
+    name: "Turbo Controller ⏩",
+    description:
+      "Makes your grandmas more efficient by replacing mashing buttons with holding them.",
+    cost: 1000,
+    rate: 50,
+    count: 0,
+  },
+  {
+    name: "Button Mashing Machine 🤖",
+    description:
+      "Much faster than you or your grandmas at mashing those buttons.",
+    cost: 2000,
+    rate: 100,
+    count: 0,
+  },
+  {
+    name: "Ludwig 👨",
+    description: "Self-proclaimed best button masher in the world.",
+    cost: 10000,
+    rate: 1000,
+    count: 0,
+  },
 ];
-
 
 //important variables
 let counter: number = 0;
 let autoClickRateMS: number = 0; //1000 means 1 click per sec
 const AUTO_CLICK_MS = 1000;
-let autoClickStrength : number = 0;
+let autoClickStrength: number = 0;
 
 //HELPER FUNCTIONS====================================================================================================================================================
 
@@ -31,31 +64,47 @@ function incrementButtonsClicks() {
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-function upgradeRate(i: number, _event : Event) {
+function upgradeRate(i: number, _event: Event) {
   const index: number = i;
   counter -= availableItems[index].cost;
-  
+
   resourceValueCounter.innerHTML = `${Math.floor(counter)} buttons mashed`;
   if (autoClickRateMS == 0) autoClickRateMS = AUTO_CLICK_MS;
   autoClickStrength += availableItems[index].rate;
   //im not sure if my prettier broke, or if the indentations below are acceptable
-growthRate.innerHTML = `${autoClickStrength * (autoClickRateMS / 1000)} buttons being mashed per second`;
-availableItems[index].count++;
-availableItems[index].cost = availableItems[index].cost * 1.15;
+  growthRate.innerHTML = `${autoClickStrength * (autoClickRateMS / 1000)} buttons being mashed per second`;
+  availableItems[index].count++;
+  availableItems[index].cost = availableItems[index].cost * 1.15;
 
-const upgradeButton = document.getElementById(`Buy ${availableItems[index].name}`) as HTMLButtonElement;
-const upgradeButtonScreen = document.getElementById(`${availableItems[index].name}`);
-if(upgradeButtonScreen) upgradeButtonScreen.innerHTML = `${availableItems[index].count} ${availableItems[index].name} owned - ${availableItems[index].description}`;
-upgradeButton.innerHTML = `Buy ${availableItems[index].name}: ${availableItems[index].cost} mashes`;
-
+  const upgradeButton = document.getElementById(
+    `Buy ${availableItems[index].name}`,
+  ) as HTMLButtonElement;
+  const upgradeButtonScreen = document.getElementById(
+    `${availableItems[index].name}`,
+  );
+  if (upgradeButtonScreen)
+    upgradeButtonScreen.innerHTML = `${availableItems[index].count} ${availableItems[index].name} owned - ${availableItems[index].description}`;
+  upgradeButton.innerHTML = `Buy ${availableItems[index].name}: ${availableItems[index].cost} mashes`;
 }
-
 
 //INNER HTML SETUP==================================================================================================================================================
 const app: HTMLDivElement = document.querySelector("#app")!;
+document.title = "My button mashing game";
+const header = document.createElement("h1");
+header.innerHTML = "My button mashing game";
+app.append(header);
+
+//used brace for this https://chat.brace.tools/s/aa2a1e9a-e4f8-44fb-9dd7-385748f720b5
+const button: HTMLButtonElement = document.createElement("bigButton") as HTMLButtonElement;
+button.innerHTML = "Mash Button 🎮";
+button.id = "Button Masher";
+button.style.cssText = "color: black; background-color: white";
+app.append(button);
+
 const resourceValue: HTMLDivElement = document.querySelector("#app")!;
-const resourceValueCounter = document.createElement("p");
+const resourceValueCounter = document.createElement("h2");
 const growthRate = document.createElement("p");
+resourceValue.append(resourceValueCounter, growthRate);
 
 availableItems.forEach((item, index) => {
   const upgradeCountScreen = document.createElement("p");
@@ -66,30 +115,19 @@ availableItems.forEach((item, index) => {
   upgradeCountScreen.innerHTML = `${item.count} ${item.name} owned - ${item.description}`;
   upgradeButton.innerHTML = `Buy ${item.name}: ${item.cost} mashes`;
   upgradeButton.id = buttonId;
-  upgradeButton.style.cssText = "color: white; background-color: black; text-decoration: line-through;";
+  upgradeButton.style.cssText =
+    "color: white; background-color: black; text-decoration: line-through;";
   upgradeButton.disabled = true;
   app.append(upgradeCountScreen, upgradeButton);
 
-  upgradeButton.addEventListener("click", (event) => upgradeRate(index, event), false);
+  upgradeButton.addEventListener(
+    "click",
+    (event) => upgradeRate(index, event),
+    false,
+  );
 });
-resourceValue.append(resourceValueCounter, growthRate);
-
-
-document.title = "My button mashing game";
-const header = document.createElement("h1");
-header.innerHTML = "My button mashing game";
-app.append(header);
-//used brace for this https://chat.brace.tools/s/aa2a1e9a-e4f8-44fb-9dd7-385748f720b5
-const button: HTMLButtonElement = document.createElement("button");
-button.innerHTML = "Mash Button 🎮";
-button.id = "Button Masher";
-button.style.cssText = "color: black; background-color: white";
-app.append(button);
-
-
 
 //MAIN LOOP========================================================================================================================================================
-
 
 //used mdn docs example for this part https://developer.mozilla.org/en-US/docs/Web/API/Window/requestAnimationFrame#examples
 let start: DOMHighResTimeStamp;
@@ -108,22 +146,22 @@ function incrementButtonsTime(timestamp: DOMHighResTimeStamp) {
     lastElapsed = elapsed;
   }
 
- availableItems.forEach((item) => {
-  //end part 'as HTMLButtonElement' cleared an error, got it from brace https://chat.brace.tools/s/7d0d16ca-d3cf-464f-b35f-44e49a6098e1
-    const upgradeButton = document.getElementById(`Buy ${item.name}`) as HTMLButtonElement;
-    if(counter >= item.cost) {
-      upgradeButton.style.textDecoration = 'none';
+  availableItems.forEach((item) => {
+    //end part 'as HTMLButtonElement' cleared an error, got it from brace https://chat.brace.tools/s/7d0d16ca-d3cf-464f-b35f-44e49a6098e1
+    const upgradeButton = document.getElementById(
+      `Buy ${item.name}`,
+    ) as HTMLButtonElement;
+    if (counter >= item.cost) {
+      upgradeButton.style.textDecoration = "none";
       upgradeButton.disabled = false;
-    }
-    else {
-      upgradeButton.style.textDecoration = 'line-through';
+    } else {
+      upgradeButton.style.textDecoration = "line-through";
       upgradeButton.disabled = true;
     }
- });
+  });
 
   requestAnimationFrame(incrementButtonsTime);
 }
-
 
 document.body.append(resourceValue);
 
